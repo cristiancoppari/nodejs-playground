0387272f1e4634a177e2923ede3152d35b48dd1f
Cristian <cristian.coppari@gmail.com>
2025-01-04 15:06:18 -0300

    feat: add tests

--- src/app.ts
+++ src/app.ts
@@ -10,10 +10,10 @@ import { buildLogger } from "./plugins/logger.plugin";
 
 // const getPokemonById = require('./js-foundation/06-promises');
 
-const logger = buildLogger("app.js");
+// const logger = buildLogger("app.js");
 
-logger.log("Hola mundo");
-logger.error("Esto es algo malo");
+// logger.log("Hola mundo");
+// logger.error("Esto es algo malo");
 
 // getPokemonById(4)
 //   .then( ( pokemon ) => console.log({ pokemon }) )
