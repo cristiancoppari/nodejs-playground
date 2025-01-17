7a6d65941df790f0993ba090e54a234a137943f1
Cristian <cristian.coppari@gmail.com>
2025-01-17 14:34:23 -0300

    feat: add env test

--- src/config/plugins/env.plugin.ts
+++ src/config/plugins/env.plugin.ts
@@ -4,7 +4,7 @@ import * as env from "env-var";
 dotenv.config();
 
 export const envs = {
-  PORT: env.get("PORT").required().asString(),
+  PORT: env.get("PORT").required().asInt(),
   MAILER_SERVICE: env.get("MAILER_SERVICE").required().asString(),
   MAILER_EMAIL: env.get("MAILER_EMAIL").required().asString(),
   MAILER_SECRET_KEY: env.get("MAILER_SECRET_KEY").required().asString(),
