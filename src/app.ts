7b3f44f14aa18da6edca6eb0e8ca04bfc1fa5879
Cristian <cristian.coppari@gmail.com>
2025-01-20 22:33:44 -0300

    feat: add express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import { Server } from "./presentation/server";
+import { envs } from "./config/env";
+
+(() => {
+  main();
+})();
+
+function main() {
+  const server = new Server({
+    port: envs.PORT,
+    publicPath: envs.PUBLIC_PATH,
+  });
+  server.start();
+}
