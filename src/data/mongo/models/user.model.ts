d3067779f00aa6bc10ca6b402e2f1ad0fb5a9c90
Cristian <cristian.coppari@gmail.com>
2025-01-23 19:04:52 -0300

    feat: add user model

---
+++ src/data/mongo/models/user.model.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: [true, "Name is required"] },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: { type: String, required: [true, "Password is required"] },
+  img: { type: String },
+  role: {
+    type: [String],
+    enum: {
+      values: ["ADMIN_ROLE", "USER_ROLE"],
+    },
+    default: "USER_ROLE",
+  },
+});
+
+export const UserModel = mongoose.model("User", userSchema);
