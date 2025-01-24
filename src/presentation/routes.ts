e0620cd92e9d37602a6a794532d43f3d170fddaa
Cristian <cristian.coppari@gmail.com>
2025-01-24 17:39:31 -0300

    feat: add routes and controllers

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { AuthRoutes } from "./auth/routes";
-
+import { CategoryRoutes } from "./category/routes";
 export class AppRoutes {
   static get routes(): Router {
     const router = Router();
 
     // Definir las rutas
     router.use("/api/auth", AuthRoutes.routes);
+    router.use("/api/category", CategoryRoutes.routes);
 
     return router;
   }
